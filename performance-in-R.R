24d6c0cdb199a3eaadee250ecc4bde745f23847b
Rod Morgado <rodrigo@zooft.company>
2022-01-09 13:00:08 -0800

    replacing && by & allows us not evaluating all the logic statements when the fist one is false

--- performance-in-R.R
+++ performance-in-R.R
@@ -39,3 +39,25 @@ microbenchmark(
     app_sol = app(rolls),
     r_sum_sol = r_sum(rolls)
 )
+
+# Example data
+is_double
+
+# Define the previous solution
+move <- function(is_double) {
+    if (is_double[1] & is_double[2] & is_double[3]) {
+        current <- 11 # Go To Jail
+    }
+}
+
+# Define the improved solution
+improved_move <- function(is_double) {
+    if (is_double[1] && is_double[2] && is_double[3]) {
+        current <- 11 # Go To Jail
+    }
+}
+
+# microbenchmark both solutions
+# Very occassionally the improved solution is actually a little slower
+# This is just random chance
+microbenchmark(move(is_double), improved_move(is_double), times = 1e5)
