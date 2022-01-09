a9dd1f63e78498ecadaec981daaa2342a57ff2e5
Rod Morgado <rodrigo@zooft.company>
2022-01-09 12:56:57 -0800

    optimizing a function to sum the rows. Use rowSums() instead apply()

--- performance-in-R.R
+++ performance-in-R.R
@@ -20,3 +20,22 @@ microbenchmark(
  data.frame_solution = d(),
  matrix_solution     = m()
 )
+
+# Example data
+rolls
+
+# Define the previous solution 
+app <- function(x) {
+    apply(x, 1, sum)
+}
+
+# Define the new solution
+r_sum <- function(x) {
+    rowSums(x)
+}
+
+# Compare the methods
+microbenchmark(
+    app_sol = app(rolls),
+    r_sum_sol = r_sum(rolls)
+)
