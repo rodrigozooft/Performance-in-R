7d7f55e3e146f1eea16edfa7269a41c97b0e6fba
Rod Morgado <rodrigo@zooft.company>
2022-01-09 12:55:03 -0800

    using microbenchmark to compare a dataframe generatin against the matrix

---
+++ performance-in-R.R
@@ -0,0 +1,22 @@
+# Load the microbenchmark package
+library(microbenchmark)
+
+# The previous data frame solution is defined
+# d() Simulates 6 dices rolls
+d <- function() {
+  data.frame(
+    d1 = sample(1:6, 3, replace = TRUE),
+    d2 = sample(1:6, 3, replace = TRUE)
+  )
+}
+
+# Complete the matrix solution
+m <- function() {
+  matrix(sample(1:6, 6, replace = TRUE), ncol = 2)
+}
+
+# Use microbenchmark to time m() and d()
+microbenchmark(
+ data.frame_solution = d(),
+ matrix_solution     = m()
+)
